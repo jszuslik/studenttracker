245517a20d9d93392bb8b45c585b84438a7c503a
Joshua Szuslik <joshuaszuslik@gmail.com>
2016-10-20 01:48:26 -0500

    Working update

--- student-api/service/src/main/java/com/norulesweb/studenttracker/api/web/user/UserWebServiceImpl.java
+++ student-api/service/src/main/java/com/norulesweb/studenttracker/api/web/user/UserWebServiceImpl.java
@@ -1,10 +1,8 @@
 package com.norulesweb.studenttracker.api.web.user;
 
 import com.norulesweb.studenttracker.api.services.user.UserWebService;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/test")
 public class UserWebServiceImpl extends UserWebService {
 }
